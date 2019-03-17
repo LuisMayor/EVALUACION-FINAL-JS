2fe89b99ecd12f4736e7adb2d61a3542a308c6ba
Luis <39141191+LuisMayor@users.noreply.github.com>
2019-03-17 12:42:09 -0400

    Declaracion de Variables

--- WEB16S_C4_codBase/codBase/js/app.js
+++ WEB16S_C4_codBase/codBase/js/app.js
@@ -1,3 +1,23 @@
-function init() {
 
-}
+//declaracion de variables
+var display= document.getElementById("display");
+var on = document.getElementById("on");
+var sign = document.getElementById("sign");
+var raiz = document.getElementById("raiz")
+var dividido = document.getElementById("dividido");
+var siete = document.getElementById("7");
+var ocho = document.getElementById("8");
+var nueve = document.getElementById("9");
+var por = document.getElementById("por");
+var cuatro = document.getElementById("4");
+var cinco = document.getElementById("5");
+var seis = document.getElementById("6");
+var menos = document.getElementById("menos");
+var uno = document.getElementById("1");
+var dos = document.getElementById("2");
+var tres = document.getElementById("3");
+var cero = document.getElementById("0");
+var punto = document.getElementById("punto");
+var igual = document.getElementById("igual");
+var mas = document.getElementById("mas");
+
