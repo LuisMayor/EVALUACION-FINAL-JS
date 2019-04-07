3f9f033a21c46a3459e21ef3f9b0cdddec9255dd
Luis <39141191+LuisMayor@users.noreply.github.com>
2019-04-07 13:58:31 -0400

    se agrego los botones del uno al cero en el display y se muestran

--- WEB16S_C4_codBase/codBase/js/app.js
+++ WEB16S_C4_codBase/codBase/js/app.js
@@ -1,23 +1,91 @@
 
-//declaracion de variables
-var display= document.getElementById("display");
-var on = document.getElementById("on");
-var sign = document.getElementById("sign");
-var raiz = document.getElementById("raiz")
-var dividido = document.getElementById("dividido");
-var siete = document.getElementById("7");
-var ocho = document.getElementById("8");
-var nueve = document.getElementById("9");
-var por = document.getElementById("por");
-var cuatro = document.getElementById("4");
-var cinco = document.getElementById("5");
-var seis = document.getElementById("6");
-var menos = document.getElementById("menos");
-var uno = document.getElementById("1");
-var dos = document.getElementById("2");
-var tres = document.getElementById("3");
-var cero = document.getElementById("0");
-var punto = document.getElementById("punto");
-var igual = document.getElementById("igual");
-var mas = document.getElementById("mas");
+var calculadora={
+    //declaracion de variables
+ display: document.getElementById("display"),
+ on : document.getElementById("on"),
+ sign : document.getElementById("sign"),
+ raiz : document.getElementById("raiz"),
+ dividido : document.getElementById("dividido"),
+ siete : document.getElementById("7"),
+ ocho : document.getElementById("8"),
+ nueve : document.getElementById("9"),
+ por : document.getElementById("por"),
+ cuatro : document.getElementById("4"),
+ cinco : document.getElementById("5"),
+ seis : document.getElementById("6"),
+ menos : document.getElementById("menos"),
+ uno : document.getElementById("1"),
+ dos : document.getElementById("2"),
+ tres : document.getElementById("3"),
+ cero : document.getElementById("0"),
+ punto : document.getElementById("punto"),
+ igual : document.getElementById("igual"),
+ mas : document.getElementById("mas"),
+     
+ init:function(){    //con el metodo init le doy inicio a mis funciones 
+    this.uno.addEventListener('click',function(){    //eventos listener para mostrar mis numeros en pantalla 
+    display.textContent += "1";
+    
+    })
+    this.dos.addEventListener('click',function(){
+    display.textContent += "2";
+        
+    })
+    this.tres.addEventListener('click',function(){
+    display.textContent += "3";
+            
+    })
+    this.cuatro.addEventListener('click',function(){
+    display.textContent += "4";
+                
+    })
+
+    this.cinco.addEventListener('click',function(){
+    display.textContent += "5";
+        
+    })
+    this.seis.addEventListener('click',function(){
+    display.textContent += "6";
+                    
+    })
+    this.siete.addEventListener('click',function(){
+    display.textContent += "7";
+                        
+    })
+        
+    this.ocho.addEventListener('click',function(){
+    display.textContent += "8";
+                
+    })
+
+    this.nueve.addEventListener('click',function(){
+    display.textContent += "9";
+                    
+    })
+    this.cero.addEventListener('click',function(){
+            display.textContent += "0";
+                        
+    })
+    this.on.addEventListener('click',function(){
+    display.textContent = "0";
+                                
+    })       
+
+    this.mas.addEventListener('click',function(){
+    display.textContent += "+";
+                    
+    })
+    }
+
+    
+
+
+}
+
+calculadora.init();
+
+
+
+
+
 
